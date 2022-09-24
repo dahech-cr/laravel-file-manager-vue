deb56b29a0491b1cf338dde2dc65b7425e763e2f
dahech-cr <vitaliy.berezovskiy137@gmail.com>
2022-09-24 21:48:37 +0300

    fixed config for npm

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,54 @@
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [vue()],
+//     build: {
+//         minify: true,
+//         cssCodeSplit: false,
+//         rollupOptions: {
+//             output: {
+//                 manualChunks: {
+//                     vendor: [],
+//                 },
+//             },
+//         },
+//     },
+
+//     css: { preprocessorOptions: { scss: { charset: false } } },
+// });
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     build: {
         minify: true,
         cssCodeSplit: false,
+        lib: {
+            entry: resolve(__dirname, 'src/init.js'),
+            name: 'laravelFileManager',
+            // the proper extensions will be added
+            fileName: 'laravel-file-manager',
+        },
         rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
             output: {
-                manualChunks: {
-                    vendor: [],
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
                 },
+                // manualChunks: {
+                //     vendor: [],
+                // },
             },
         },
     },
-
     css: { preprocessorOptions: { scss: { charset: false } } },
-});
+})
