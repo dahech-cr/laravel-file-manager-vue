9d00f466dbaadfa17886298d9f1c859e20acf665
dahech-cr <vitaliy.berezovskiy137@gmail.com>
2022-10-03 23:05:36 +0300

    rename package

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [vue()],
-//     build: {
-//         minify: true,
-//         cssCodeSplit: false,
-//         rollupOptions: {
-//             output: {
-//                 manualChunks: {
-//                     vendor: [],
-//                 },
-//             },
-//         },
-//     },
-
-//     css: { preprocessorOptions: { scss: { charset: false } } },
-// });
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
@@ -30,23 +9,16 @@ export default defineConfig({
         cssCodeSplit: false,
         lib: {
             entry: resolve(__dirname, 'src/init.js'),
-            name: 'laravelFileManager',
+            name: 'laravelFileManagerVue3',
             // the proper extensions will be added
-            fileName: 'laravel-file-manager',
+            fileName: 'laravel-file-manager-vue3',
         },
         rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
             external: ['vue'],
             output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
                 globals: {
                     vue: 'Vue',
                 },
-                // manualChunks: {
-                //     vendor: [],
-                // },
             },
         },
     },
